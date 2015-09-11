756428dd4043d62da7431fa3693ee9590720e925
senning <senning@puppydogtales.ca>
2015-09-11 10:43:10 -0400

    compass config

--- config.rb
+++ config.rb
@@ -1,12 +1,14 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "./"
+http_path = (environment == :production) ? "https://aaf1a18515da0e792f78-c27fdabe952dfc357fe25ebf5c8897ee.ssl.cf5.rackcdn.com/1955/" : "./" 
 css_dir = "css"
 sass_dir = "scss"
-images_dir = "./img"
+images_dir = "img"
 javascripts_dir = "js"
-fonts_dir = "./fonts"
+fonts_dir = "fonts"
+relative_assets = (environment == :production) ? false : true
+
 
 #include bootstrap partials for import
 add_import_path "vendor/bootstrap/assets/stylesheets"
