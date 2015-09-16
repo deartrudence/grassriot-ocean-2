9dce3417484d829a4694c36914e1777cbdf3b3eb
senning <senning@puppydogtales.ca>
2015-09-16 10:34:55 -0400

    raygun added

--- js/1.1.js
+++ js/1.1.js
@@ -1,1 +1,55 @@
-webpackJsonp([1],{15:function(e,t,n){function o(e){return n(r(e))}function r(e){return i[e]||function(){throw new Error("Cannot find module '"+e+"'.")}()}var i={"./GRAnalytics-pixel-facebook":16,"./GRAnalytics-pixel-facebook.js":16};o.keys=function(){return Object.keys(i)},o.resolve=r,e.exports=o,o.id=15},16:function(e,t,n){(function(t){function n(e,n){fb_param.pixel_id=e,n instanceof Object&&(fb_param.value=n.revenue,fb_param.currency=n.currency),t.getScript(path)}window.fb_param={},path="//connect.facebook.net/en_US/fp.js",e.exports={showPixel:n}}).call(t,n(1))}});
+webpackJsonp([1],{
+
+/***/ 15:
+/***/ function(module, exports, __webpack_require__) {
+
+	var map = {
+		"./GRAnalytics-pixel-facebook": 16,
+		"./GRAnalytics-pixel-facebook.js": 16
+	};
+	function webpackContext(req) {
+		return __webpack_require__(webpackContextResolve(req));
+	};
+	function webpackContextResolve(req) {
+		return map[req] || (function() { throw new Error("Cannot find module '" + req + "'.") }());
+	};
+	webpackContext.keys = function webpackContextKeys() {
+		return Object.keys(map);
+	};
+	webpackContext.resolve = webpackContextResolve;
+	module.exports = webpackContext;
+	webpackContext.id = 15;
+
+
+/***/ },
+
+/***/ 16:
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function($) {/**
+	 * GRAnalytics-pixel-facebook module.
+	 * 
+	 * specific implementation for facebook pixel (old method)
+	 * 
+	 */
+
+	window.fb_param = {};
+	path = '//connect.facebook.net/en_US/fp.js';
+
+	function showPixel(id, transactionData) {
+	    fb_param.pixel_id = id; 
+	    if(transactionData instanceof Object) {
+	        fb_param.value = transactionData.revenue; 
+	        fb_param.currency = transactionData.currency;
+	    }
+	    $.getScript(path);
+	}
+
+	module.exports = {
+	    showPixel: showPixel
+	};
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }
+
+});
